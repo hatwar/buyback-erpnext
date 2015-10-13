7fd4bff9b498391f99444ec979c0de683f5c1fd7
Gangadhar Kadam <gangadhar.k@indictanstech.com>
2015-10-13 19:17:59 +0530

    on frappe 6

--- erpnext/buying/doctype/purchase_order/purchase_order.py
+++ erpnext/buying/doctype/purchase_order/purchase_order.py
@@ -240,6 +240,40 @@ def set_missing_values(source, target):
 
 @frappe.whitelist()
 def make_purchase_receipt(source_name, target_doc=None):
+	return new_purchase_receipt(source_name,target_doc=None)
+	# def update_item(obj, target, source_parent):
+	# 	target.qty = flt(obj.qty) - flt(obj.received_qty)
+	# 	target.stock_qty = (flt(obj.qty) - flt(obj.received_qty)) * flt(obj.conversion_factor)
+	# 	target.amount = (flt(obj.qty) - flt(obj.received_qty)) * flt(obj.rate)
+	# 	target.base_amount = (flt(obj.qty) - flt(obj.received_qty)) * \
+	# 		flt(obj.rate) * flt(source_parent.conversion_rate)
+
+	# doc = get_mapped_doc("Purchase Order", source_name,	{
+	# 	"Purchase Order": {
+	# 		"doctype": "Purchase Receipt",
+	# 		"validation": {
+	# 			"docstatus": ["=", 1],
+	# 		}
+	# 	},
+	# 	"Purchase Order Item": {
+	# 		"doctype": "Purchase Receipt Item",
+	# 		"field_map": {
+	# 			"name": "prevdoc_detail_docname",
+	# 			"parent": "prevdoc_docname",
+	# 			"parenttype": "prevdoc_doctype",
+	# 		},
+	# 		"postprocess": update_item,
+	# 		"condition": lambda doc: doc.received_qty < doc.qty
+	# 	},
+	# 	"Purchase Taxes and Charges": {
+	# 		"doctype": "Purchase Taxes and Charges",
+	# 		"add_if_empty": True
+	# 	}
+	# }, target_doc, set_missing_values)
+
+	# return doc
+
+def new_purchase_receipt(source_name,target_doc=None):
 	def update_item(obj, target, source_parent):
 		target.qty = flt(obj.qty) - flt(obj.received_qty)
 		target.stock_qty = (flt(obj.qty) - flt(obj.received_qty)) * flt(obj.conversion_factor)
@@ -250,6 +284,9 @@ def make_purchase_receipt(source_name, target_doc=None):
 	doc = get_mapped_doc("Purchase Order", source_name,	{
 		"Purchase Order": {
 			"doctype": "Purchase Receipt",
+			"field_map": {
+				# "warehouse": "warehouse",
+			},
 			"validation": {
 				"docstatus": ["=", 1],
 			}
@@ -260,6 +297,7 @@ def make_purchase_receipt(source_name, target_doc=None):
 				"name": "prevdoc_detail_docname",
 				"parent": "prevdoc_docname",
 				"parenttype": "prevdoc_doctype",
+				"serial_no":"serial_no",
 			},
 			"postprocess": update_item,
 			"condition": lambda doc: doc.received_qty < doc.qty
@@ -269,6 +307,9 @@ def make_purchase_receipt(source_name, target_doc=None):
 			"add_if_empty": True
 		}
 	}, target_doc, set_missing_values)
+	pr = doc.save()
+	doc.submit()
+	msgprint(_("{0} is Created Successfully.").format(doc.name))
 
 	return doc
 
